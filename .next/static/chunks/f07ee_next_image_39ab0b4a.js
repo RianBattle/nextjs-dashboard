e00f7119fc1a34dde8b110f9600aa7b4c7a1bec4
RianBattleSKL <rbattle@skltd.com>
2025-06-11 13:06:25 -0400

    chapter 9

---
+++ .next/static/chunks/f07ee_next_image_39ab0b4a.js
@@ -0,0 +1,11 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([typeof document === "object" ? document.currentScript : undefined, {
+
+"[project]/node_modules/.pnpm/next@15.3.2_react-dom@19.1.0_react@19.1.0__react@19.1.0/node_modules/next/image.js [app-client] (ecmascript)": (function(__turbopack_context__) {
+
+var { g: global, __dirname, m: module, e: exports } = __turbopack_context__;
+{
+module.exports = __turbopack_context__.r("[project]/node_modules/.pnpm/next@15.3.2_react-dom@19.1.0_react@19.1.0__react@19.1.0/node_modules/next/dist/shared/lib/image-external.js [app-client] (ecmascript)");
+}}),
+}]);
+
+//# sourceMappingURL=f07ee_next_image_39ab0b4a.js.map
---
+++ app/dashboard/(overview)/page.tsx
@@ -0,0 +1,34 @@
+import { Card } from "@/app/_ui/dashboard/cards";
+import RevenueChart from "@/app/_ui/dashboard/revenue-chart";
+import LatestInvoices from "@/app/_ui/dashboard/latest-invoices";
+import { lusitana } from "@/app/_ui/fonts";
+import { Suspense } from "react";
+import CardWrapper from "@/app/_ui/dashboard/cards";
+import {
+    RevenueChartSkeleton,
+    LatestInvoicesSkeleton,
+    CardSkeleton
+} from "@/app/_ui/skeletons";
+
+export default async function Page() {
+    return (
+        <main>
+            <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+                Dashboard
+            </h1>
+            <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+                <Suspense fallback={<CardSkeleton />}>
+                    <CardWrapper />
+                </Suspense>
+            </div>
+            <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+                <Suspense fallback={<RevenueChartSkeleton />}>
+                    <RevenueChart />
+                </Suspense>
+                <Suspense fallback={<LatestInvoicesSkeleton />}>
+                    <LatestInvoices />
+                </Suspense>
+            </div>
+        </main>
+    );
+}
